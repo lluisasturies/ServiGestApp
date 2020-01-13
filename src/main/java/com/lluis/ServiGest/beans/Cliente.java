96441961750bb3c0906354a95adab1510ce7028e
Lluis <lluisasturies@gmail.com>
2020-01-13 13:58:53 +0100

    Creadas las primeras relaciones de Clientes y Viviendas

--- src/main/java/com/lluis/ServiGest/beans/Cliente.java
+++ src/main/java/com/lluis/ServiGest/beans/Cliente.java
@@ -1,9 +1,36 @@
 package com.lluis.ServiGest.beans;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clientes")
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer idCliente;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "idPropietario")
+    private List<Vivienda> viviendasPropietario;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "idInquilino")
+    private List<Vivienda> viviendasInquilino;
+	
+	@Column
 	private String nombre;
+	
+	@Column
 	private String apellidos;
+	
+	@Column
 	private String dni;
 }
--- src/main/java/com/lluis/ServiGest/beans/Vivienda.java
+++ src/main/java/com/lluis/ServiGest/beans/Vivienda.java
@@ -1,12 +1,38 @@
 package com.lluis.ServiGest.beans;
 
-public class Vivienda {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="viviendas")
+public class Vivienda {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer idVivienda;
-	private Integer idPropietario; // Cliente asignado como propietario
-	private Integer idInquilino; // Cliente asignado como inquilino
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idPropietario")
+	private Cliente idPropietario; // Cliente asignado como propietario
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idInquilino")
+	private Cliente idInquilino; // Cliente asignado como inquilino
+	
+	@Column
 	private String direccion;
+	
+	@Column
 	private String localidad;
+	
+	@Column
 	private String provincia;
 	
 }
