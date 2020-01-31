0cf09c0e04d544ca0cb4c676444cfc2728e55cd9
Lluis <lluisasturies@gmail.com>
2020-01-31 14:41:35 +0100

    Mejora

--- src/main/java/com/lluis/ServiGest/RutasInicio.java
+++ src/main/java/com/lluis/ServiGest/RutasInicio.java
@@ -1,17 +1,34 @@
 package com.lluis.ServiGest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lluis.ServiGest.datos.clientes.ClienteDAO;
+import com.lluis.ServiGest.datos.ordenes.OrdenDAO;
+import com.lluis.ServiGest.datos.viviendas.ViviendaDAO;
+
 @Controller
 public class RutasInicio {
 
+	@Autowired
+	private ViviendaDAO viviendaDAO;
+	
+	@Autowired
+	private ClienteDAO clienteDAO;
+	
+	@Autowired
+	private OrdenDAO ordenDAO;
+	
 	@GetMapping("/")
 	public ModelAndView inicio() {
 		
 		ModelAndView model = new ModelAndView();
 		model.setViewName("inicio"); // Nombre de la vista
+		model.addObject("numeroViviendas", viviendaDAO.count());
+		model.addObject("numeroClientes", clienteDAO.count());
+		model.addObject("numeroOrdenes", ordenDAO.count());
 		
 		return model;
 	}
