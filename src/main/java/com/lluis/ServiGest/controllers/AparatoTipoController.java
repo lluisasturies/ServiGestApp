7353c226c34d185a52c688d68d37cdbad97c9d9d
Lluis <lluisasturies@gmail.com>
2020-06-06 15:07:51 +0200

    Creado Controlador AparatoTipo

---
+++ src/main/java/com/lluis/ServiGest/controllers/AparatoTipoController.java
@@ -0,0 +1,69 @@
+package com.lluis.ServiGest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lluis.ServiGest.pojos.AparatoTipo;
+import com.lluis.ServiGest.servicios.AparatoTipoService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("api/aparatos/tipos")
+public class AparatoTipoController {
+	
+	@Autowired
+	AparatoTipoService aparatoTipoService;
+	
+	// Obtener LISTA de Tipos
+	@GetMapping
+	@PreAuthorize("hasRole('TECNICO') or hasRole('ADMIN')")
+	public List<AparatoTipo> listaMarcasAparatos(){
+		return aparatoTipoService.listaTiposAparatos();
+	}
+	
+	// Obtener UN Tipo
+	@GetMapping("/{idTipo}")
+	@PreAuthorize("hasRole('TECNICO') or hasRole('ADMIN')")
+	public AparatoTipo verAparato(@PathVariable("idTipo") Integer idTipo) {
+		return aparatoTipoService.verTipoAparato(idTipo);
+	}
+	
+	// ADD
+	@PostMapping("/add")
+	@PreAuthorize("hasRole('ADMIN')")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void add(@RequestBody AparatoTipo aparatoMarca) {
+		aparatoTipoService.add(aparatoMarca);
+	}
+	
+	// UPDATE
+	@PutMapping("/update")
+	@PreAuthorize("hasRole('ADMIN')")
+	@ResponseStatus(HttpStatus.OK)
+	public void update(@RequestBody AparatoTipo aparatoTipo) {
+		aparatoTipoService.update(aparatoTipo);
+	}
+	
+	// DELETE
+	@DeleteMapping("/delete/{idTipo}")
+	@PreAuthorize("hasRole('ADMIN')")
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@PathVariable("idTipo") Integer idTipo) {
+		aparatoTipoService.delete(idTipo);
+	}
+
+}
