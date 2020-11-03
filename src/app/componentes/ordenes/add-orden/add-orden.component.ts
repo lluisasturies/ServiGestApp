363623331704659772dd82f259255d9a57351f86
Lluis <lluisasturies@gmail.com>
2020-11-03 11:04:58 +0100

    ReactiveForms que faltaban

--- src/app/componentes/ordenes/add-orden/add-orden.component.ts
+++ src/app/componentes/ordenes/add-orden/add-orden.component.ts
@@ -1,3 +1,4 @@
+import { Vivienda } from 'src/app/modelos/Vivienda.model';
 import { Component, OnInit, Input } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
@@ -9,6 +10,7 @@ import { EmpresasService } from 'src/app/servicios/empresas.service';
 // Modelos
 import { Orden } from 'src/app/modelos/Orden.model';
 import { Empresa } from 'src/app/modelos/Empresa.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-orden',
@@ -18,9 +20,11 @@ import { Empresa } from 'src/app/modelos/Empresa.model';
 export class AddOrdenComponent implements OnInit {
 
   // Variables
-  @Input() public vivienda;
+  @Input() public vivienda: Vivienda;
+
   public orden: Orden = new Orden();
   public empresas: Empresa[];
+  public ordenForm: FormGroup;
   public fechaActual = formatDate(new Date(), 'yyyy-MM-dd', 'en-EN');
 
   constructor(
@@ -30,8 +34,17 @@ export class AddOrdenComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.orden.estado = 1;
     this.obtenerEmpresas();
+
+    // Creo el FormGroup
+    this.ordenForm = new FormGroup({
+      vivienda: new FormControl(this.vivienda),
+      empresa: new FormControl('', Validators.required),
+      orden: new FormControl('', Validators.required),
+      asunto: new FormControl('', Validators.required),
+      fecha: new FormControl(this.fechaActual),
+      estado: new FormControl(1, Validators.required)
+    });
   }
 
   // Obtener lista de Empresas
