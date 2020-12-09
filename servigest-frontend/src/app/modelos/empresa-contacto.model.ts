24e1f53b3d13065fd0db30790a8f40a622ce1a9c
unknown <lluisasturies@gmail.com>
2020-12-09 10:26:03 +0100

    Creado el empresaContactoService

--- servigest-frontend/src/app/modelos/empresa-contacto.model.ts
+++ servigest-frontend/src/app/modelos/empresa-contacto.model.ts
@@ -2,7 +2,7 @@ import { Cliente } from './cliente.model';
 import { Empresa } from './Empresa.model';
 import { TipoContacto } from './tipo-contacto.model';
 
-export class ClienteContacto {
+export class EmpresaContacto {
     idContacto: number;
     empresa: Empresa;
     tipo: TipoContacto;
