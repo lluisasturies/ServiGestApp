4107c44a4a3b165f0bdf9163af11ff55b7b98f90
unknown <lluisasturies@gmail.com>
2020-12-14 09:42:53 +0100

    Perfeccionando el modulo de Aparatos

--- servigest-frontend/src/app/aparatos/componentes/aparatos.component.ts
+++ servigest-frontend/src/app/aparatos/componentes/aparatos.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AparatoMarca } from 'src/app/modelos/aparato-marca.model';
+import { AparatoTipo } from 'src/app/modelos/aparato-tipo.model';
 
 import { Aparato } from 'src/app/modelos/aparato.model';
+import { AparatosMarcasService } from 'src/app/servicios/aparatos-marcas.service';
+import { AparatosTiposService } from 'src/app/servicios/aparatos-tipos.service';
 
 import { AparatosService } from 'src/app/servicios/aparatos.service';
 import { ConfirmationDialogService } from 'src/app/servicios/confirmation-dialog.service';
@@ -20,16 +24,22 @@ export class AparatosComponent implements OnInit {
   // Variables
   public dtOptions: DataTables.Settings = {};
   public aparatos: Aparato[];
+  public marcas: AparatoMarca[];
+  public tipos: AparatoTipo[];
 
   constructor(
     private _aparatos: AparatosService,
+    private _tipos: AparatosTiposService,
+    private _marcas: AparatosMarcasService,
     private modalService: NgbModal,
     private confirmationDialogService: ConfirmationDialogService
   ) { }
 
   ngOnInit(): void {
     this.opcionesDt();
-    this.obtenerAparatos();
+    this.obtenerTipos();
+    this.obtenerMarcas();
+    this.obtenerAparatos();    
   }
 
   // Opciones del DataTable
@@ -38,6 +48,20 @@ export class AparatosComponent implements OnInit {
       pageLength: 25, // Registros por pagina
       language: { url: '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json' } // Español
     };
+  }
+
+  // Obtener todos los Tipos
+  obtenerTipos(): void {
+    this._tipos.getTipos().subscribe(data => {
+      this.tipos = data;
+    });
+  }
+
+  // Obtener todas las Marcas
+  obtenerMarcas(): void {
+    this._marcas.getMarcas().subscribe(data => {
+      this.marcas = data;
+    });
   }
 
   // Obtener todos los Aparatos
