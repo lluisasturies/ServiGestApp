d8b5a247ecb2c7352330ec8b55279b3686e46a58
unknown <lluisasturies@gmail.com>
2020-12-15 11:10:20 +0100

    Mejorado el selector de roles

--- servigest-frontend/src/app/usuarios/componentes/update-usuario/update-usuario.component.ts
+++ servigest-frontend/src/app/usuarios/componentes/update-usuario/update-usuario.component.ts
@@ -3,6 +3,8 @@ import { Usuario } from 'src/app/modelos/nuevo-usuario.model';
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Rol } from 'src/app/modelos/rol.model';
+import { RolesService } from 'src/app/servicios/roles.service';
 
 @Component({
   selector: 'app-update-usuario',
@@ -14,16 +16,21 @@ export class UpdateUsuarioComponent implements OnInit {
   // Variables
   @Input() usuario: Usuario;
 
+  public roles: Rol[] = [];
+
   public usuarioForm: FormGroup;
   public isFail = false;
   public errorMsg = '';
 
   constructor(
     public _usuarios: UsuariosService,
+    private _roles: RolesService,
     public modalService: NgbActiveModal
   ) { }
 
   ngOnInit(): void {
+    this.obtenerRoles();
+    
     // Creo el FormGroup
     this.usuarioForm = new FormGroup({
       id: new FormControl(this.usuario.id),
@@ -31,6 +38,16 @@ export class UpdateUsuarioComponent implements OnInit {
       email: new FormControl(this.usuario.email, Validators.required),
       rol: new FormControl(this.usuario.rol)
     });
+  }
+
+  // Comparador del Formulario
+  comparaRol(c1: Rol, c2: Rol): boolean
+  {
+    return c1 && c2 ? c1.rolNombre === c2.rolNombre : c1 === c2;
+  }
+
+  obtenerRoles(): void {
+    this._roles.getRoles().subscribe(data => { this.roles = data; });
   }
 
   // Actualizar el Usuario
